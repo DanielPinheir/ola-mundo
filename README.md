fbb0d6ef92a42249bf8232f91199e417543fff00
Daniel Pinheiro Machado <dyelzim@gmail.com>
2023-05-26 22:29:21 -0300

    Update Rodape - add github and linkedin icons

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Hello world project
 
 > Project carried out in React where a SPA (simple page application) will be created with cards from articles about technology.
+
+# Tasks
+
+### Components:
+
+[x] Create Menu component <br>
+[x] Create Banner component <br>
+[x] Create MenuLink component with _useLocation()_ <br>
+[x] Create Main Page component using one native method of React-Router-Dom called Outlet<br>
+[x] Create PostCard componentusing one native method of React-Router-Dom called Link<br>
+[x] Create PostModelo component with Props <br>
+[x] Create Rodape component <br>  
+[x] Create component <br>
+
+### Pages:
+
+[x] Create Start page <br>
+[x] Create Aboute me page <br>
+[x] Create Post page with useParams Hook<br>
+
+### Routes:
+
+[x] Create routes using a native methods of React-Router-Dom called BrowserRouter, Routes and Route<br>
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,11 +1,28 @@
 import styles from "./Rodape.module.css";
 import { ReactComponent as MarcaRegistrada } from "../../assets/marca_registrada.svg";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
       <MarcaRegistrada />
-      Desenvolvido por Alura.
+      Desenvolvido por Daniel Pinheiro
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/DanielPinheir"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/daniel-pinheiro-machado/"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
     </footer>
   );
 }
